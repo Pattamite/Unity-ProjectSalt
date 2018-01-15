0c41cee044261852218f8ba6c51e910a63219d55
Pattnatt <jittapat007@gmail.com>
2018-01-15 11:38:44 +0700

    Update ButtonsController

--- ProjectSalt/Assets/Scripts/ButtonsController.cs
+++ ProjectSalt/Assets/Scripts/ButtonsController.cs
@@ -7,9 +7,19 @@ public class ButtonsController : MonoBehaviour {
 
     // Use this for initialization
     public GameObject currentSelectedButton;
+    private bool isActive;
+    public bool isMainButtonSet;
+
+    private static float activePosition = 69.4f;
+    private static float notActivePosition = -500f;
 
 	void Start () {
-		
+        if (!isMainButtonSet) {
+            Deactivate();
+        }
+        else {
+            Activate();
+        }
 	}
 	
 	// Update is called once per frame
@@ -31,6 +41,13 @@ public class ButtonsController : MonoBehaviour {
         }
     }
 
+    public void DeSelectAllButton () {
+        foreach(Transform child in transform) {
+            MarkAsNotSelect(child.gameObject);
+        }
+        currentSelectedButton = null;
+    }
+
     private void MarkAsSelect (GameObject gameObject) {
         Color color = gameObject.GetComponent<Image>().color;
         gameObject.GetComponent<Image>().color = new Color(color.r, color.g, color.b, 0.5f);
@@ -39,5 +56,22 @@ public class ButtonsController : MonoBehaviour {
     private void MarkAsNotSelect (GameObject gameObject) {
         Color color = gameObject.GetComponent<Image>().color;
         gameObject.GetComponent<Image>().color = new Color(color.r, color.g, color.b, 1f);
+    }
+
+    public void Activate () {
+        isActive = true;
+        Vector3 pos = GetComponent<RectTransform>().anchoredPosition;
+        GetComponent<RectTransform>().anchoredPosition = new Vector3(pos.x, activePosition, pos.z);
+    }
+
+    public void Deactivate () {
+        isActive = false;
+        Vector3 pos = GetComponent<RectTransform>().anchoredPosition;
+        GetComponent<RectTransform>().anchoredPosition = new Vector3(pos.x, notActivePosition, pos.z);
+        DeSelectAllButton();
+    }
+
+    public bool GetIsActive () {
+        return isActive;
     }
 }
