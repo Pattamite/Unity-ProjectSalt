1331bda0309a1e16dcdab87931701d955ccb1359
Pattnatt <jittapat007@gmail.com>
2018-02-04 00:07:48 +0700

    Finalizing Main Menu

---
+++ ProjectSalt/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour {
+
+    private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+        audioSource = GetComponent<AudioSource>();
+        if (AudioController.IsMusicEnable()) audioSource.Play();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void LoadLevel (int value) {
+        audioSource.Stop();
+        SceneManager.LoadScene(value);
+    }
+}
